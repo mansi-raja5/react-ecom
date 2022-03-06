b258d17f6b89384f0e08f25e3d289f8974d82b5f
Mansi Raja <mansi.raja@encora.com>
2022-03-06 14:19:55 +0530

    Mini Cart Design Part -1 - ignore

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./style.scss";
 import logo from "./../../assets/logo.png";
+import Button from "../Button";
 
 import { connect } from "react-redux";
 
 const Header = ({ cart }) => {
   const [cartCount, setCartCount] = useState(0);
+  const [showMiniCart, setShowMiniCart] = useState(0);
 
   useEffect(() => {
     let count = 0;
@@ -17,6 +19,10 @@ const Header = ({ cart }) => {
     setCartCount(count);
   }, [cart, cartCount]);
 
+  const showMiniCartHandler = () => {
+    console.log("showMiniCartHandler");
+    setShowMiniCart(showMiniCart == 0 ? 1 : 0);
+  };
   return (
     <header className="header">
       <div className="container">
@@ -56,14 +62,55 @@ const Header = ({ cart }) => {
         <div className="actions">
           <ul>
             <li>
-              <NavLink
-                to="/cart"
+              <Button
                 className="cart"
                 aria-label="View your shopping cart"
+                onClick={showMiniCartHandler}
               >
                 <i className="fas fa fa-shopping-cart fa-lg"></i>
                 <span className="badge"> {cartCount} </span>
-              </NavLink>
+              </Button>
+              <div
+                className="overlay"
+                style={{ display: showMiniCart ? "block" : "none" }}
+              >
+                <div className="container">
+                  <div className="mini-cart">
+                    <ul className="mini-cart-items">
+                      <li className="clearfix">
+                        <img
+                          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/cart-item1.jpg"
+                          alt="item1"
+                        />
+                        <span className="item-name">Sony DSC-RX100M III</span>
+                        <span className="item-price">$849.99</span>
+                        <span className="item-quantity">Quantity: 01</span>
+                      </li>
+                    </ul>
+                    <div className="row">
+                      <div className="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                        <NavLink
+                          to="/cart"
+                          className="cart"
+                          aria-label="View your shopping cart"
+                          onSelect={showMiniCartHandler}
+                        >
+                          <Button>View Bag</Button>
+                        </NavLink>
+                      </div>
+                      <div className="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                        <NavLink
+                          to="/cart"
+                          className="cart"
+                          aria-label="View your shopping cart"
+                        >
+                          <Button>Checkout</Button>
+                        </NavLink>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </li>
           </ul>
         </div>
