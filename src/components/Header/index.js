974419690cc03db721d5e5237b606fe2618d839a
Mansi Raja <mansi.raja@encora.com>
2022-03-06 23:09:25 +0530

    Mini Cart Design Part - 2

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./style.scss";
 import logo from "./../../assets/logo.png";
-import Button from "../Button";
+import MiniCart from "../MiniCart";
 
 import { connect } from "react-redux";
 
@@ -20,7 +20,6 @@ const Header = ({ cart }) => {
   }, [cart, cartCount]);
 
   const showMiniCartHandler = () => {
-    console.log("showMiniCartHandler");
     setShowMiniCart(showMiniCart == 0 ? 1 : 0);
   };
   return (
@@ -62,55 +61,18 @@ const Header = ({ cart }) => {
         <div className="actions">
           <ul>
             <li>
-              <Button
+              <a
                 className="cart"
                 aria-label="View your shopping cart"
                 onClick={showMiniCartHandler}
               >
                 <i className="fas fa fa-shopping-cart fa-lg"></i>
                 <span className="badge"> {cartCount} </span>
-              </Button>
-              <div
-                className="overlay"
-                style={{ display: showMiniCart ? "block" : "none" }}
-              >
-                <div className="container">
-                  <div className="mini-cart">
-                    <ul className="mini-cart-items">
-                      <li className="clearfix">
-                        <img
-                          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/cart-item1.jpg"
-                          alt="item1"
-                        />
-                        <span className="item-name">Sony DSC-RX100M III</span>
-                        <span className="item-price">$849.99</span>
-                        <span className="item-quantity">Quantity: 01</span>
-                      </li>
-                    </ul>
-                    <div className="row">
-                      <div className="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-                        <NavLink
-                          to="/cart"
-                          className="cart"
-                          aria-label="View your shopping cart"
-                          onSelect={showMiniCartHandler}
-                        >
-                          <Button>View Bag</Button>
-                        </NavLink>
-                      </div>
-                      <div className="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-                        <NavLink
-                          to="/cart"
-                          className="cart"
-                          aria-label="View your shopping cart"
-                        >
-                          <Button>Checkout</Button>
-                        </NavLink>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              </a>
+              <MiniCart
+                onVeiwBagData={showMiniCartHandler}
+                showMiniCart={showMiniCart}
+              />
             </li>
           </ul>
         </div>
